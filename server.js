a8d990168ba322cb78d7e2a90c9b71ded5fdd4c5
Sam Smiley <samtsmiley@gmail.com>
2018-11-13 12:40:20 -0800

    added some errs

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ app.get('/api/notes', (req, res) => {
     res.json(data);
   }
 });
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
 
 app.get('/api/notes/:id', (req, res) => {
   const id = req.params.id; 
@@ -32,6 +35,18 @@ app.get('/api/notes/:id', (req, res) => {
   res.json(data.find(item => item.id === Number(id)));
 });
 
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
 
 app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
