b97d69f3c518f0747b5aa46d8e27af38b3dfd776
Sam Smiley <samtsmiley@gmail.com>
2018-11-14 13:36:34 -0800

    added router mod

--- server.js
+++ server.js
@@ -5,77 +5,26 @@
 console.log('Hello Noteful!');
 
 const express = require('express');
-const data = require('./db/notes');
-const simDB = require('./db/simDB');  // <<== add this
-const notes = simDB.initialize(data); // <<== and this
-const {PORT} = require('./config');
-const {logger} = require('./middleware/logger');
 
+// const data = require('./db/notes');
+// const simDB = require('./db/simDB');  // <<== add this
+// const notes = simDB.initialize(data); // <<== and this
+// const {logger} = require('./middleware/logger');
+
+const {PORT} = require('./config');
+const morgan = require('morgan');
 const app = express();
-app.use(logger);
+const notesRouter = require('./router/notes.router');
+
+app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use(express.json());
 
 
-app.get('/api/notes', (req, res, next) => {
-  const { searchTerm } = req.query;
-  notes.filter(searchTerm, (err, list) => {
-    if (err) {
-      return next(err); // goes to error handler
-    }
-    res.json(list); // responds with filtered array
-  });
-});
+app.use('/api/notes/', notesRouter);
 
-app.get('/api/notes/:id', (req, res, next) => {
-  const {id} = req.params; 
-  notes.find(id, (err, list) => {
-    if (err) {
-      return next(err); // goes to error handler
-    }
-    res.json(list); // responds with filtered array
-  });
-});
 
-app.put('/api/notes/:id', (req, res, next) => {
-  const id = req.params.id;
 
-  /***** Never trust users - validate input *****/
-  const updateObj = {};
-  const updateFields = ['title', 'content'];
-
-  updateFields.forEach(field => {
-    if (field in req.body) {
-      updateObj[field] = req.body[field];
-    }
-  });
-
-  notes.update(id, updateObj, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      console.log(req.body);
-      console.log(updateObj);
-      res.json(item);
-    } else {
-      next();
-    }
-  });
-});
-
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  res.status(404).json({ message: 'Not Found' });
-});
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: err
-  });
-});
 
 app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
