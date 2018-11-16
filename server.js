7d6f7a133e88bc994cfb4ac5804dfba29a147d3f
Sam Smiley <samtsmiley@gmail.com>
2018-11-16 11:46:56 -0800

    first tests made

--- server.js
+++ server.js
@@ -25,9 +25,11 @@ app.use('/api/notes', notesRouter);
 
 
 
-
-app.listen(PORT, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
-});
+if (require.main === module) {
+  app.listen(PORT, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
+}
+module.exports = app; // Export for testing
